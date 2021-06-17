ce1a113cbc721740f2cded27261f45100da47b46
Rohit Saxena <34920137+rohit290554@users.noreply.github.com>
2021-06-17 18:01:24 +0530

    Update dbConnection.php

--- dbConnection.php
+++ dbConnection.php
@@ -1,5 +1,5 @@
 <?php
 //all the variables defined here are accessible in all the files that include this one
-$con= new mysqli('https://www.db4free.net/','projtest1','qwertyui','oexam_test')or die("Could not connect to mysql".mysqli_error($con));
+$con= new mysqli('db4free.net','projtest1','qwertyui','oexam_test')or die("Could not connect to mysql".mysqli_error($con));
 
 ?>
