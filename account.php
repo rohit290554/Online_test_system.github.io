2da2a3f620cb730249b21c43d52ddc9a4a00ac7d
garvit-droid <85953937+garvit-droid@users.noreply.github.com>
2021-06-19 20:07:46 +0530

    Update account.php

--- account.php
+++ account.php
@@ -138,7 +138,8 @@ echo '<div class="panel"<a title="Back to Archive" href="update.php?q1=2"><b><sp
 
 <span id="countdown" class="timer"></span>
 <script>
-var seconds = 40;
+echo 'time left';
+ var seconds = 40;
     function secondPassed() {
     var minutes = Math.round((seconds - 30)/60);
     var remainingSeconds = seconds % 60;
